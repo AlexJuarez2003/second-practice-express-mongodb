5eab5b2f99a6104f7dd54595fcf1a60647344f9f
Alexander <alexanderjuarez0055@gmail.com>
2026-07-02 20:27:23 -0600

    CORS implemented

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import express from "express";
 import userRoutes from "./routes/user.routes.js";
 import articleRoutes from "./routes/article.routes.js";
 import clientRoutes from "./routes/client.routes.js";
+import cors from "cors";
 
 const app = express();
 
@@ -9,5 +10,6 @@ app.use(express.json());
 app.use(userRoutes);
 app.use(articleRoutes);
 app.use(clientRoutes);
+app.use(cors());
 
 export default app;
